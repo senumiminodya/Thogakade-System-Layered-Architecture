8ef6df0a3e2aee84ce90c1c415b6cfe885bf7651
senumiminodya <senumiminodya456@gmail.com>
2023-12-15 10:17:13 +0530

    Manage Customers

---
+++ src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
@@ -0,0 +1,25 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class CustomerDAOImpl {
+    public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ArrayList<CustomerDTO> getAllCustomer = new ArrayList<>();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+
+        while (rst.next()) {
+            CustomerDTO customerDTO = new CustomerDTO(rst.getString("id"), rst.getString("name"), rst.getString("address"));
+            getAllCustomer.add(customerDTO);
+        }
+        return getAllCustomer;
+    }
+}
